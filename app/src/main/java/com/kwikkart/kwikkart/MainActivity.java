02ac1092b3dcd0b10012259def04d038e48bdcb7
Gabriel Morales <da.gabrielmora@gmail.com>
2019-11-28 23:11:33 -0600

    Added preliminary splash page

--- app/src/main/java/com/kwikkart/kwikkart/MainActivity.java
+++ app/src/main/java/com/kwikkart/kwikkart/MainActivity.java
@@ -2,13 +2,34 @@ package com.kwikkart.kwikkart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button lButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       getWindow().setStatusBarColor(Color.rgb(100 , 193, 255));
+        lButton = findViewById(R.id.loginButton);
+
+
     }
+
+
+    public void loginButtonClicked(View view) {
+
+
+
+    }
+
+
 }
