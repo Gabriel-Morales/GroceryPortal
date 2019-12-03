09b203fa257bf106a886bd6e90b0be01177ab369
Gabriel Morales <da.gabrielmora@gmail.com>
2019-12-03 13:00:35 -0600

    changed design a bit, fixed crash.

--- app/src/main/java/com/kwikkart/kwikkart/Home.java
+++ app/src/main/java/com/kwikkart/kwikkart/Home.java
@@ -1,6 +1,7 @@
 package com.kwikkart.kwikkart;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -8,6 +9,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,16 +18,21 @@ import androidx.navigation.ui.NavigationUI;
 
 public class Home extends AppCompatActivity {
 
+    private Toolbar homeToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        getWindow().setStatusBarColor(Color.WHITE);
+        homeToolbar = findViewById(R.id.home_toolbar);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+        setSupportActionBar(homeToolbar);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_cart, R.id.navigation_track).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        //NavigationUI .setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI .setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
