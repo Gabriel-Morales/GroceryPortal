5d64e4bf38b649bd2158db0bccf6248bdd8ae1c1
Gabriel Morales <da.gabrielmora@gmail.com>
2019-12-05 18:33:15 -0600

    All data now loads in the "All" view: Images, Descriptions, etc.

--- app/src/main/java/com/kwikkart/kwikkart/ViewHolder.java
+++ app/src/main/java/com/kwikkart/kwikkart/ViewHolder.java
@@ -1,11 +1,21 @@
 package com.kwikkart.kwikkart;
 
 import android.net.Uri;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 /**
  *  This class is intended to represent the items that will be held within the recycler view.
@@ -17,19 +27,36 @@ public class ViewHolder extends RecyclerView.ViewHolder {
     private TextView name;
     private TextView price;
 
+
     public ViewHolder(View view)
     {
         super(view);
         image = view.findViewById(R.id.itemImage);
         name = view.findViewById(R.id.itemName);
         price = view.findViewById(R.id.itemPrice);
+
     }
 
     public void initializeView(String name, String price, String imagePath)
     {
         this.name.setText(name);
         this.price.setText(price);
-        //this.image.setImageURI(Uri.parse(""));
+
+        StorageReference storageReference = FirebaseStorage.getInstance().getReferenceFromUrl(imagePath);
+
+        storageReference.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>()  {
+
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+
+                if (task.isSuccessful())
+                {
+                     Glide.with(image.getContext()).load(task.getResult().toString()).into(image);
+                }
+
+            }
+
+        });
     }
 
 
