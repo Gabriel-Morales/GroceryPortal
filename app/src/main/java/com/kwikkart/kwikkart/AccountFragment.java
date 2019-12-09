cdb1d9f5301d0a21e6afb3b7ffe749d8bf0df7da
Waewarin Chindarassami <w.chindarassami@gmail.com>
2019-12-09 12:39:27 -0600

    Fix the account layout so that the user can see what they type

--- app/src/main/java/com/kwikkart/kwikkart/AccountFragment.java
+++ app/src/main/java/com/kwikkart/kwikkart/AccountFragment.java
@@ -26,7 +26,7 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
